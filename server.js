2fc1f1c83dca162adb56447a82742d772506ddcd
Waseem Maya <waseemmayaa@gmail.com>
2019-01-13 15:30:41 +0500

    Ready for awesome2

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require("express");
 const app = express();
 const mongoose = require("./config/db");
 var cors = require("cors");
+const path = require("path");
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "client/build")));
 
 const port = process.env.PORT || 5000;
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 
 app.listen(port, () => {
   console.log(`=======> My Server is running on port ${port} <=======`);
