f0b2a605b033a6b22a33526448253f730350a6d7
Waseem Maya <waseemmayaa@gmail.com>
2019-01-13 15:12:25 +0500

    Initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-
-router.use('/users', require('./users'));
+router.use("/", require("./tasks"));
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,8 +6,10 @@ var cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-app.listen(5000, () => {
-  console.log("=======> My Server is running on port 5000 <=======");
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`=======> My Server is running on port ${port} <=======`);
 });
 
 app.use(function(req, res, next) {
